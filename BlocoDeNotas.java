7eee7353c66770e177099b078d450c1d2aad6c58
Rafael <Rafael>
2016-03-06 11:12:39 -0300

    primeiro commit

---
+++ BlocoDeNotas.java
@@ -0,0 +1,14 @@
+package blocodenotas;
+
+import java.util.Scanner;
+
+public class BlocoDeNotas {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        
+        
+    }
+    
+}
